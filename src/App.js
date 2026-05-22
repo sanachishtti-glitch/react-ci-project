46e937f1d483150fe2606e5dfc1be978472c899e
sana <sanachishtti@gmail.com>
2026-05-22 21:07:20 +0500

    initial react project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return(
+  <>
+
+
+      <h1>🚀 My First React App</h1>
+      <p>I am learning CI/CD and GitHub Actions</p>
+  
+
+  </>
+  )
+}
+
+export default App;
